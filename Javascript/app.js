2bb70b219338ead9db4bd88170a91bc27a24b4ad
fidhone <fidhone@gmail.com>
2022-09-29 19:46:58 +0200

    1ª Actualizacion

---
+++ Javascript/app.js
@@ -0,0 +1,6 @@
+/*-------------------------------------------
+-----------**Iteración #1: Variables**-------
+-------------------------------------------*/
+
+let myFavoriteHero = "Hulk";
+console.log(myFavoriteHero);
