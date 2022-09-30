cb21afcde21ae48b78c092714aa76f2154d08a58
fidhone <fidhone@gmail.com>
2022-09-30 20:42:32 +0200

    continuacion ejercicios viernes 30092022

--- Javascript/app.js
+++ Javascript/app.js
@@ -31,5 +31,22 @@ console.log(result);
 const toy1 = {name: 'Buss myYear', price: 19};
 const toy2 = {name: 'Rallo mcKing', price: 29};
 
-const totalPrice = (toy1(19) && toy2(29));
+const totalPrice = (toy1.price + toy2.price);
+
 console.log(totalPrice);
+
+
+
+let globalBasePrice = "10000";
+
+const car1 = {name: "BMW m&m", basePrice: 50000, finalPrice: 60000};
+const car2 = {name: "Chevrolet Corbina", basePrice: 70000, finalPrice: 80000};
+
+globalBasePrice = "25000";
+
+car1.basePrice + globalBasePrice;
+car2.basePrice + globalBasePrice;
+
+console.log(globalBasePrice);
+
+
