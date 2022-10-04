e571855ce5b7aa4c7acdfecf5c36797170fd8830
fidhone <fidhone@gmail.com>
2022-10-04 17:40:58 +0200

    ejercicios resueltos hasta bucles

--- Javascript/app.js
+++ Javascript/app.js
@@ -37,12 +37,12 @@ console.log(totalPrice);
 
 
 
-let globalBasePrice = "10000";
+let globalBasePrice = 10000;
 
 const car1 = {name: "BMW m&m", basePrice: 50000, finalPrice: 60000};
 const car2 = {name: "Chevrolet Corbina", basePrice: 70000, finalPrice: 80000};
 
-globalBasePrice = "25000";
+globalBasePrice = 25000;
 
 const newValue1 = (car1.basePrice + globalBasePrice);
 const newValue2 = (car2.basePrice + globalBasePrice);
@@ -141,5 +141,57 @@ console.log(rickAndMortyCharacters2);
 /*-----------------------------------------------------
 ---------**Iteración #5: Condicionales**---------------
 -----------------------------------------------------*/
+
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+if (number2 /= number1) {
+    console.log("number2 dividido entre number1 es igual a 2");
+}
+
+//-------------------------------------------------
+
+
+if (number1 !== number2) {
+    console.log("number1 es estrictamente distinto a number2");
+}
+  
+
+//-------------------------------------------------
+
+
+if (number3 != number1) {
+    console.log("number3 es distinto number1");
+}
+
+
+//--------------------------------------------------
+
+
+if (number3 *5 == number1) {
+    console.log("number3 por 5 es igual a number1");
+}
+
+
+//---------------------------------------------------
+
+
+if (number3 *5 == number1 && number1 *2 == number2) {
+    console.log("number3 por 5 es igual a number1 Y number1 por 2 es igual a number2");
+}
+
+
+//----------------------------------------------------
+
+
+if (number2 /2 == number1 || number1 /5 == number3) {
+    console.log("number2 entre 2 es igual a number1 O number1 entre 5 es igual a number3");
+}
+
+
+/*-----------------------------------------------------
+-------------**Iteración #6: Bucles**------------------
+------------------------------------------------------*/
 
 
