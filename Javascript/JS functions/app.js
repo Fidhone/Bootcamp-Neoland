63db6e6e44557b4101d1c7f2b77ed5765717c867
fidhone <fidhone@gmail.com>
2022-10-06 20:05:33 +0200

    iteration#1

---
+++ Javascript/JS functions/app.js
@@ -0,0 +1,18 @@
+/*-----------------------------------------------------
+-------------**Iteración #1: Buscar el máximo**--------
+------------------------------------------------------*/
+
+function sum(numberOne , numberTwo) {
+
+    console.log(numberOne > numberTwo);
+
+  }
+
+sum(64, 74);
+
+
+function pokemonAttack() {
+    console.log( '¡Pikachu Impact Trueno!');
+  }
+
+  pokemonAttack();
