34387eb5e192eef77b9c81c8551f92d77680f3d0
fidhone <fidhone@gmail.com>
2022-11-24 19:36:11 +0100

    ejercicios CSS

---
+++ Javascript/JS-Dom/app.js
@@ -0,0 +1,12 @@
+const showBtn = document.querySelector(`.showme`)
+console.log(showBtn)
+
+//-------------------------------------------------
+
+const showId = document.querySelector(`#pillado`)
+console.log(showId)
+
+//-------------------------------------------------
+
+const showP = document.querySelector(`p`)
+console.log(showP)
